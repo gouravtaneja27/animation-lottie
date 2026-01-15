bc9029861c30c65c9d720884b417d9cbff573eba
Gourav Taneja <gouravtaneja@Gouravs-MacBook-Air.local>
2026-01-15 11:53:31 +0530

    updated

--- app.js
+++ app.js
@@ -17,3 +17,29 @@ function submitForm() {
         path: "https://assets10.lottiefiles.com/packages/lf20_touohxv0.json"
     });
 }
+
+function submitForm() {
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if (name === "" || email === "" || password === "") {
+        alert("Please fill all fields!");
+        return; // Yahan function ruk jayega
+    }
+
+   
+    document.getElementById("form-box").style.display = "none";
+
+   
+    document.getElementById("success-box").style.display = "block";
+
+    lottie.loadAnimation({
+        container: document.getElementById("success-animation"),
+        renderer: "svg",
+        loop: false,
+        autoplay: true,
+        path: "https://assets10.lottiefiles.com/packages/lf20_touohxv0.json"
+    });
+}
